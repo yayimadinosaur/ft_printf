4af26bc1d4152a05a2f58bd91687a74223a3a28d
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-19 18:17:17 -0700

    wrote a main, need to understand stdarg to test (can write simpler main to test for now

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:50:51 by wfung             #+#    #+#             */
-/*   Updated: 2017/04/19 17:54:06 by wfung            ###   ########.fr       */
+/*   Updated: 2017/04/19 18:16:37 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,22 @@
 int		main(int ac, char **av)
 {
 	int		i;
+	char	*builtin;
+	char	*made;
 
 	i = 0;
 	if (ac > 1)
 	{
 		while (i < ac && ft_printf() > 0)
 		{
-
+			builtin = ft_strdup(atoi(av[i + 1]));
+			made = ft_strdup(atoi(av[i + 1]));
+			if (ft_strcmp(builtin, made) != 0)
+				printf("av[%i] failed builtin [%s] made [%s]\n", i + 1, builtin, made);
+			free(builtin);
+			free(made);
+			builtin = NULL;
+			made = NULL;
 			i++;
 		}
 	}
