96a7b9ec18be88fd2999d16f9a70a6a5ca68036d
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-24 16:41:38 -0700

    made logic to logic.c for colors

---
+++ logic_printf.c
@@ -0,0 +1,23 @@
+man 3 stdarg
+
+va_arg	-	creates list of the args (probably in struct)
+			inside va_list
+va_copy	-	copies list from src to dest
+
+va_arg	-	checks type of result of va_copy
+				if fail return 0?
+va_end	-	finished with current arg
+
+
+
+
+//mandatory
+You have to recode the libc’s printf function.
+• Your function will be called ft_printf and will be prototyped similarly to printf.
+• You won’t do the buffer management in the printf function.
+• You have to manage the following conversions: sSpdDioOuUxXcC
+• You must manage %%
+• You must manage the flags #0-+ and space
+• You must manage the minimum field-width
+• You must manage the precision
+• You must manage the flags hh, h, l, ll, j, et z.
