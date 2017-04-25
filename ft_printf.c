84f1979be053c37ce5a3cb373dfcbe99b199231c
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-25 08:45:30 -0700

    added a bit not much

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,29 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:16:09 by wfung             #+#    #+#             */
-/*   Updated: 2017/04/24 19:08:58 by wfung            ###   ########.fr       */
+/*   Updated: 2017/04/25 07:17:23 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+
+
 int		ft_printf(const char *format, ...)
 {
 	va_list args;
+	int		d;
+	char	c;
+	char	*str;
+
 	va_start(args, format);
 
 	while (*format != '\0')
 	{
-		va_start();
-		va_arg();
+		va_arg(arg, );
 		va_copy();
 		*format++;
 	}
-	va_end();
+	va_end(args);
 	return (1);
 }
