d4d636568b4d244a38eb689ffe3f4d20031e8e15
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-25 20:37:20 -0700

    did some work lol

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:16:09 by wfung             #+#    #+#             */
-/*   Updated: 2017/04/25 07:17:23 by wfung            ###   ########.fr       */
+/*   Updated: 2017/04/25 20:20:10 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@
 
 int		ft_printf(const char *format, ...)
 {
-	va_list args;
+	va_list arg;
+	va_list arg2;
 	int		d;
 	char	c;
 	char	*str;
 
 	va_start(args, format);
-
+	va_copy(arg2, arg);
 	while (*format != '\0')
 	{
-		va_arg(arg, );
+		va_arg(arg, );		//va_arg type should correspond to %variable
 		va_copy();
 		*format++;
 	}
-	va_end(args);
+	va_end(arg);
 	return (1);
 }
